1ef8e55e5905b5158e2ee6d6e79321466c2873ce
José Milton <j1000tonm.jmb@gmail.com>
2021-07-15 17:19:16 -0300

    Backend project created

---
+++ backend/src/main/java/com/j1000tonm/dscatalog3/Dscatalog3Application.java
@@ -0,0 +1,13 @@
+package com.j1000tonm.dscatalog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dscatalog3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dscatalog3Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/j1000tonm/dscatalog3/Dscatalog3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j1000tonm.dscatalog3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dscatalog3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
