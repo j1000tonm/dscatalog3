310056b6bd3b46280b6d755e8d6a148a3ea32615
José Milton <j1000tonm.jmb@gmail.com>
2021-10-08 15:47:10 -0300

    Find Category by id, exception handling

---
+++ backend/src/main/java/com/j1000tonm/dscatalog3/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,10 @@
+package com.j1000tonm.dscatalog3.services.exceptions;
+
+public class EntityNotFoundException extends RuntimeException	 {
+	private static final long serialVersionUID = 1L;
+	
+	public EntityNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
