621bab1a08ea911caa9fb108548004626c752843
José Milton <j1000tonm.jmb@gmail.com>
2021-10-11 15:29:14 -0300

    Delete category

--- backend/src/main/java/com/j1000tonm/dscatalog3/services/CategoryService.java
+++ backend/src/main/java/com/j1000tonm/dscatalog3/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.j1000tonm.dscatalog3.dto.CategoryDTO;
 import com.j1000tonm.dscatalog3.entities.Category;
 import com.j1000tonm.dscatalog3.repositories.CategoryRepository;
+import com.j1000tonm.dscatalog3.services.exceptions.DatabaseException;
 import com.j1000tonm.dscatalog3.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -52,6 +55,18 @@ public class CategoryService {
 		}
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} 
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 }
---
+++ backend/src/main/java/com/j1000tonm/dscatalog3/services/exceptions/DatabaseException.java
@@ -0,0 +1,10 @@
+package com.j1000tonm.dscatalog3.services.exceptions;
+
+public class DatabaseException extends RuntimeException	 {
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+
+}
