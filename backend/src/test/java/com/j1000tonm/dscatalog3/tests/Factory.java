b7591850cfe0d60eac59c8d847df2281933b27cb
José Milton <j1000tonm.jmb@gmail.com>
2022-02-16 15:10:28 -0300

    ORM mapping, database seeding

---
+++ backend/src/test/java/com/j1000tonm/dscatalog3/tests/Factory.java
@@ -0,0 +1,25 @@
+package com.j1000tonm.dscatalog3.tests;
+
+import java.time.Instant;
+
+import com.j1000tonm.dscatalog3.dto.ProductDTO;
+import com.j1000tonm.dscatalog3.entities.Category;
+import com.j1000tonm.dscatalog3.entities.Product;
+
+public class Factory {
+
+	public static Product createProduct() {
+		Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2020-10-20T03:00:00Z"));
+		product.getCategories().add(createCategory());
+		return product;
+	}
+	
+	public static ProductDTO createProductDTO() {
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+	}
+	
+	public static Category createCategory() {
+		return new Category(1L, "Electronics");
+	}
+}
