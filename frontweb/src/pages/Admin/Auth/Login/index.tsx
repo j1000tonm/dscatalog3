4a6d3cb3a0ae1d6720a2c9dbce5dc52ac7630249
José Milton <j1000tonm.jmb@gmail.com>
2022-08-25 16:56:22 -0300

    Redirecting programmatically

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
 import { getAuthData, requestBackendLogin, saveAuthData } from 'util/requests';
@@ -13,7 +13,10 @@ type FormData = {
 
 const Login = () => {
   const [hasError, setHasError] = useState(false);
+
   const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
+
+  const history = useHistory();
 
   const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
@@ -23,6 +26,7 @@ const Login = () => {
         console.log('TOKEN GERADO: ' + token);
         setHasError(false);
         console.log('SUCESSO', response);
+        history.push('/admin');
       })
       .catch((error) => {
         setHasError(true);
