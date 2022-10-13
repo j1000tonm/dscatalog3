d28e8772bdc2f0b3df5f9ace4ff8e887dcab012f
José Milton <j1000tonm.jmb@gmail.com>
2022-10-13 17:21:56 -0300

    Form buttons styling

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -7,20 +7,36 @@ const Form = () => {
         <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
 
         <form action="">
-            <div className="row">
-                <div className="col-lg-6">
-                    <input type="text" className="form-control base-input" />
-                    <input type="text" className="form-control base-input" />
-                    <input type="text" className="form-control base-input" />
-                </div>
-                <div className="col-lg-6">
-                    <textarea name="" rows={10} className="form-control base-input"></textarea>
-                </div>
+          <div className="row product-card-inputs-container">
+            <div className="col-lg-6 product-card-inputs-left-container">
+              <div className="margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div className="margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div>
+                <input type="text" className="form-control base-input" />
+              </div>
             </div>
-            <div>
-                <button className="btn btn-outline-danger">CANCELAR</button>
-                <button className="btn btn-primary">SALVAR</button>
+            <div className="col-lg-6">
+              <div>
+                <textarea
+                  name=""
+                  rows={10}
+                  className="form-control base-input h-auto"
+                />
+              </div>
             </div>
+          </div>
+          <div className="product-card-buttons-container">
+            <button className="btn btn-outline-danger product-card-button">
+              CANCELAR
+            </button>
+            <button className="btn btn-primary product-card-button text-white">
+              SALVAR
+            </button>
+          </div>
         </form>
       </div>
     </div>
