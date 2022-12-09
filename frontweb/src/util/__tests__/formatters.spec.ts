d0a87de45e6b02e0edf77e2e483d4f36717cfdde
j1000tonm <j1000tonm.jmb@gmail.com>
2022-12-09 16:37:49 -0300

    REFACTOR token, Role

--- frontweb/src/util/__tests__/formatters.spec.ts
+++ frontweb/src/util/__tests__/formatters.spec.ts
@@ -1,6 +1,27 @@
 import { formatPrice } from "util/formatters"
 
-test('formatPrice should format number pt-BR when give 10.1', () => {
-  const result = formatPrice(10.1);
-  expect(result).toEqual("10,10");
+describe('formatPrice for positive numbers', () => {
+
+    test('formatPrice should format number pt-BR when give 10.1', () => {
+        const result = formatPrice(10.1);
+        expect(result).toEqual("10,10");
+      });
+
+      test('formatPrice should format number pt-BR when give 0.1', () => {
+        const result = formatPrice(0.1);
+        expect(result).toEqual("0,10");
+      });
+});
+
+describe('formatPrice for non-positive numbers', () => {
+
+    test('formatPrice should format number pt-BR when give 0', () => {
+        const result = formatPrice(0);
+        expect(result).toEqual("0,00");
+      });
+
+      test('formatPrice should format number pt-BR when give -5.1', () => {
+        const result = formatPrice(-5.1);
+        expect(result).toEqual("-5,10");
+      });
 });
