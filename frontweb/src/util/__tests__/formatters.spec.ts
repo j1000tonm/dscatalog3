3cf1e80394d5c96c80b5ffdc5413d820bb547b3b
j1000tonm <j1000tonm.jmb@gmail.com>
2022-12-12 17:49:43 -0300

    Mocking function with spyOn

--- frontweb/src/util/__tests__/formatters.spec.ts
+++ frontweb/src/util/__tests__/formatters.spec.ts
@@ -1,27 +1,14 @@
 import { formatPrice } from "util/formatters"
 
-describe('formatPrice for positive numbers', () => {
+describe('formatPrice tests', () => {
 
-    test('formatPrice should format number pt-BR when give 10.1', () => {
+    test('should format number pt-BR when give 10.1', () => {
         const result = formatPrice(10.1);
         expect(result).toEqual("10,10");
       });
 
-      test('formatPrice should format number pt-BR when give 0.1', () => {
+      test('should format number pt-BR when give 0.1', () => {
         const result = formatPrice(0.1);
         expect(result).toEqual("0,10");
-      });
-});
-
-describe('formatPrice for non-positive numbers', () => {
-
-    test('formatPrice should format number pt-BR when give 0', () => {
-        const result = formatPrice(0);
-        expect(result).toEqual("0,00");
-      });
-
-      test('formatPrice should format number pt-BR when give -5.1', () => {
-        const result = formatPrice(-5.1);
-        expect(result).toEqual("-5,10");
       });
 });
