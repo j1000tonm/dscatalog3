459754872ae88f08f27da0a5b992de6d86177d02
j1000tonm <j1000tonm.jmb@gmail.com>
2022-12-23 14:55:14 -0300

    MSW mock server, fixtures

--- frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -2,6 +2,11 @@ import { render, screen, waitFor } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import Catalog from '..';
 import history from 'util/history';
+import { server } from './fixtures';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 test('should render Catalog with products', async () => {
 
